e1c42b8cda46cad6940321e24ba286268252bae2
lijungan <48832152+lijungan@users.noreply.github.com>
2019-06-05 18:42:36 +0800

    project

---
+++ Victor/UploadFileFilter.java
@@ -0,0 +1,11 @@
+ 
+package com.util;
+
+public interface UploadFileFilter {
+    /** *//** *//** *//**
+     * ͨ���ļ����׺�ж��ļ��Ƿ񱻽���
+     * @param filename �ļ�������·��
+     * @return
+     */
+    public boolean accept(String filename);
+}
